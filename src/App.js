1544119aaa32508db7bcd6f0021763aaf48f2ad6
Randy <randy930529@gmail.com>
2022-01-27 10:27:32 -0500

    settings app react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
+import 'bulma';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <header>
+                <Link to="/"><h1 className={styles.Title}>YouTM</h1></Link>
+            </header>
+            <main>
+            <Routes>
+                <Route exact path="/movies/:movieId" />
+                <Route path="/" />
+            </Routes>
+            </main>
+        </Router>
   );
 }
 
